9de0e4e4b95e972dadae03f7416f45cac258ce13
ThomasLEMERCIER <t.lemercier27@gmail.com>
2022-04-06 20:39:21 +0200

    Initial Commit

---
+++ .env.cmake
@@ -0,0 +1,3 @@
+set(GLFW_PATH "C:/Users/Thomas/Projects/Libs/glfw-3.3.6.bin.WIN64")
+set(GLM_INCLUDE_DIRS "C:/Users/Thomas/Projects/Libs/glm")
+set(VULKAN_SDK_PATH  "C:/Users/Thomas/Projects/Libs/Vulkan")
